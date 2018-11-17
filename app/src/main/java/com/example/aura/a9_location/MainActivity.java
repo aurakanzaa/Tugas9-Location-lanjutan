9bb15288d6ac7f64ad9bedd9c6afa54efa63fbbb
aurakanzaa <aurakanzaa@yahoo.co.id>
2018-11-17 07:52:20 +0700

    9.2: Mendeteksi Lokasi dari ​ Last Known Location

--- app/src/main/java/com/example/aura/a9_location/MainActivity.java
+++ app/src/main/java/com/example/aura/a9_location/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aura.a9_location;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -9,17 +10,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
     // btn location
     public Button btnLoc;
 
+    // variable dg tipe Location
+    private Location mLastLocation;
+
+
+    // var dg tipe FusedLocationProviderClient
+    private FusedLocationProviderClient mFusedLocationClient;
+    public TextView mLocationTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationTextView = (TextView) findViewById(R.id.textMap);
 
         btnLoc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +59,29 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION} ,
                     REQUEST_LOCATION_PERMISSION );
         } else {
-            Log.d("GETPERMISSION", "getLocation : permission granted");
+//            Log.d("GETPERMISSION", "getLocation : permission granted");
+
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+//                                get lang long
+
+                                mLastLocation = location;
+                                mLocationTextView. setText(
+                                        getString(R.string.location_text,
+                                                mLastLocation.getLatitude(),
+                                                mLastLocation.getLongitude(),
+                                                mLastLocation.getTime())
+                                );
+
+                            } else {
+                                mLocationTextView.setText("Lokasi tidak tersedia");
+                            }
+                        }
+                    }
+            );
         }
 
     }
